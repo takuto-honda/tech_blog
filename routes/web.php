b32887a1fbd18b7b70e8c22cc09346c1747c7feb
honda_takuto <takuto.engineer@gmail.com>
2023-01-13 20:54:20 +0900

    RestFulなコントローラーの作成

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ContactFormController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -17,6 +18,9 @@ use Inertia\Inertia;
 */
 
 Route::group(['middleware' => 'basicauth'], function() {
+
+    Route::resource('contacts', ContactFormController::class);
+
     Route::get('/', function () {
         return Inertia::render('Welcome', [
             'canLogin' => Route::has('login'),
